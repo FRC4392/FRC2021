c1994cb3152575b8ca9ef623c261c6730efd785f
Kristopher Zakfeld <kzakfeld@gmail.com>
2021-01-20 19:26:50 -0600

    Cleanes Up

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -4,14 +4,15 @@ import edu.wpi.first.wpilibj.drive.Vector2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 
 public interface SwerveModule {
-    void setSpeed(double speed);
-    void setRotation(double rotation);
-    double getSpeed();
-    double getRotation();
-    void set(Vector2d drive);
-    Vector2d getState();
+    //Used to run any initialization needed on the swerve module
     void init();
+    //Used to log any data from the serve module
     void log();
-    void set(Translation2d drive);
+    //Used to set the velocity and position of the swerve module
+    void set(Vector2d drive);
+    //Used to get the velocity and position of the swerve module
+    Vector2d getState();
+    //Used to get the location of the swerve module relative to the 
+    //center of the robot
     Translation2d getLocation();
 }
--- src/main/java/frc/robot/swerve/SwerveModuleMk1.java
+++ src/main/java/frc/robot/swerve/SwerveModuleMk1.java
@@ -5,63 +5,83 @@ import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.drive.Vector2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+
 public class SwerveModuleMk1 implements SwerveModule {
 
     private final CANSparkMax mAzimuthMotor;
     private final CANSparkMax mDriveMotor;
     private final CANCoder mAzimuthCanCoder;
-    private final double mPositionX;
-    private final double mPositionY;
     private final CANEncoder mAzimuthEncoder;
     private final CANEncoder mDrivEncoder;
     private final CANPIDController mDrivePID;
     private final CANPIDController mAzimuthPID;
+    private final Translation2d mLocation;
 
-    public SwerveModuleMk1(CANSparkMax azimuthMotor, CANSparkMax driveMotor, CANCoder azimuthEncoder, double X, double Y){
+    public SwerveModuleMk1(CANSparkMax azimuthMotor, CANSparkMax driveMotor, CANCoder azimuthEncoder,
+            Translation2d location) {
         mAzimuthCanCoder = azimuthEncoder;
         mDriveMotor = driveMotor;
         mAzimuthMotor = azimuthMotor;
-        mPositionX = X;
-        mPositionY = Y;
+        mLocation = location;
+
         mAzimuthEncoder = mAzimuthMotor.getEncoder();
         mDrivEncoder = mDriveMotor.getEncoder();
         mDrivePID = mDriveMotor.getPIDController();
         mAzimuthPID = mAzimuthMotor.getPIDController();
     }
 
-
-    @Override
+    // Sets the drive motor speed in open loop mode
     public void setSpeed(double speed) {
         mDriveMotor.set(speed);
-
     }
 
-    @Override
+    // Sets the rotation speed of the azimuth motor in open loop mode
     public void setRotation(double rotation) {
         mAzimuthMotor.set(rotation);
+    }
 
+    // Gets the speed of the drive motor
+    public double getSpeed() {
+        return mDrivEncoder.getVelocity();
+    }
+
+    // Gets the rotation position of the azimuth module
+    public double getRotation() {
+        return mAzimuthEncoder.getPosition();
     }
 
     @Override
-    public double getSpeed() {
+    public Translation2d getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector2d getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void init() {
         // TODO Auto-generated method stub
 
     }
 
     @Override
-    public double getRotation() {
-        mAzimuthCanCoder.getAbsolutePosition();
+    public void log() {
+        // TODO Auto-generated method stub
 
     }
 
     @Override
-    public double getPositionX() {
-        return mPositionX;
+    public void set(Vector2d drive) {
+        // TODO Auto-generated method stub
+
     }
 
-    @Override
-    public double getPositionY() {
-        return mPositionY;
-    }
+
     
 }
