90db2d70bfbfc07cb7cf04c587551622ea52da1d
Project-C0D3 <67422494+Project-C0D3@users.noreply.github.com>
2021-01-20 20:09:17 -0500

    Update SwerveModule.java

--- src/main/java/frc/robot/swerve/SwerveModule.java
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -1,10 +1,18 @@
 package frc.robot.swerve;
 
+import edu.wpi.first.wpilibj.drive.Vector2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+
 public interface SwerveModule {
     void setSpeed(double speed);
     void setRotation(double rotation);
     double getSpeed();
     double getRotation();
-    double getPositionX();
-    double getPositionY();
+    void set(Vector2d drive);
+    Vector2d getState();
+    void innit();
+    void log();
+    void set(Translation2d drive);
+    Translation2d getLocation();
+    }
 }
